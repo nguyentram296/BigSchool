932e0cc717f81e583af065902d541cf6759f7600
Nguyên Trâm <nguyentram10b1@gmail.com>
2019-04-10 09:18:42 +0700

    them nut dang ky khoa hoc

--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using BigSchool.Models;
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -13,17 +14,22 @@ namespace BigSchool.Controllers
     public class AttendancesController : ApiController
     {
         private ApplicationDbContext _dbContext;
+        private int courseId;
+
         public AttendancesController()
         {
             _dbContext = new ApplicationDbContext();
         }
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exitsts!");
             var attendance = new Attendance
             {
-                CourseId = courseId,
-                AttendeeId = User.Identity.GetUserId()
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
             _dbContext.SaveChanges();
